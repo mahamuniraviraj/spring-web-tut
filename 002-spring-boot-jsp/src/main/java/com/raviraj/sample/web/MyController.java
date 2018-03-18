28f864711a8964bcd1523078a5afd6e9981a8964
Raviraj Mahamuni <mahamuniraviraj@gmail.com>
2018-03-19 00:06:39 +0530

    #2 create controller and request mapping method
    The method will handle request , prepare model and pass it view jsp page

---
+++ 002-spring-boot-jsp/src/main/java/com/raviraj/sample/web/MyController.java
@@ -0,0 +1,48 @@
+/*-
+ *  File    :   MyController.java
+ *  Version :   1.0
+ *  Date    :   Mar 18, 2018
+ *  Author  :   raviraj
+ *
+    This file is part of 002-spring-boot-jsp.
+
+    002-spring-boot-jsp is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    002-spring-boot-jsp is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with 002-spring-boot-jsp.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+package com.raviraj.sample.web;
+
+import java.time.LocalDateTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author raviraj
+ * @date   Mar 18, 2018
+ * @since  1.0
+ */
+
+@Controller
+public class MyController {
+
+	@RequestMapping("/timePlz")
+	public String timePage(Model model) {
+
+		model.addAttribute("currTime", LocalDateTime.now());
+
+		return "myTimePage";
+	}
+
+}
